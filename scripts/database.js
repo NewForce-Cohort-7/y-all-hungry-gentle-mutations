43bb3eba505b91a5055f031657d93ba930d8e2af
Harrison Davidson <hossdavidson@gmail.com>
2023-03-17 10:16:22 -0400

    HD-Java

--- scripts/database.js
+++ scripts/database.js
@@ -1,5 +1,21 @@
 const database = {
-    transientState: {}
+    transientState: {},
+    hotdogs: [
+        {id: 1, name: "Triple-H Dog", price: 300},
+        {id: 2, name: "Footlong", price: 300},
+        {id: 3, name: "Corn Dawg", price: 300},
+        {id: 4, name: "Bratwurst", price: 300},
+        {id: 5, name: "Corndog", price: 300},
+    ],
+    drinks: [
+        {id: 1, name: "Mountain Dew", price: 300},
+        {id: 2, name: "Sprite", price: 300},
+        {id: 3, name: "Coke", price: 300},
+        {id: 4, name: "Sweet Tea", price: 300},
+        {id: 5, name: "Blackberry Lemonade", price: 300},
+        {id: 6, name: "Water", price: 300},
+    ],
+    
 }
 
 export const setLocation = (locationId) => {
